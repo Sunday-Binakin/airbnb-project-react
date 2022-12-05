8a006429632919860fff9937cee6df6cf1466b28
Sunday-Binakin <Sundaybinakin@gmail.com>
2022-12-05 23:19:06 +0000

    initial commit

---
+++ lesson_1/index.js
@@ -0,0 +1,20 @@
+// ReactDOM.render(<h1>Hola,Como estas</h1>, document.getElementById("root"));
+// const navbar =(
+//     <nav>
+//     <h1>Website</h1>
+//         <ul>
+//         <li>Pricing</li>
+//         <li>About</li>
+//         <li>Contact</li>
+//             </ul>
+//     </nav>
+// )
+
+const logo = (
+   <div>
+    <img src="./React-icon.svg.png" alt="" style="width:40px" />
+   </div> 
+
+)
+// const divSelect = document.getElementById("#root");
+ReactDOM.render(logo,document.querySelector("#root"));
